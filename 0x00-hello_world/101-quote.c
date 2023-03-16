12596a038d1ca8cc49884cdc7b92296ccd2eee2a
Realnomsy <nomsy66@gmail.com>
2023-03-16 07:03:35 -0700

    Size

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - print quote
- *
- * Return: Always 1 (Success)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	writef(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
